26f2a4940b93227269439b8fd6bc04d07d5249a9
Ubuntu <ubuntu@ip-172-31-28-172.ap-northeast-1.compute.internal>
2026-01-08 03:15:55 +0000

    [create controller]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/show'
+  get 'categories/index'
+  get 'categories/show'
+  get 'posts/new'
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
 end
