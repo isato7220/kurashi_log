eb7d5059b46cbf28325437a16b114ee565ef241d
Ubuntu <ubuntu@ip-172-31-28-172.ap-northeast-1.compute.internal>
2026-03-12 02:46:20 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,35 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-
-
-
 def show
   @user = current_user
 
-
   start_date = params.fetch(:start_date, Date.today).to_date
+
   @calendar_posts = @user.posts.where(
     posted_on: start_date.beginning_of_month..start_date.end_of_month
   )
 
+  @monthly_total = @user.posts.where(
+    posted_on: start_date.beginning_of_month..start_date.end_of_month
+  ).sum(:amount)
+
+  @category_totals = @user.posts
+                        .joins(:category)
+                        .group("categories.name")
+                        .sum(:amount)
+  posts = @user.posts.includes(:category)
+  
 
   if params[:date].present?
     @selected_date = params[:date].to_date
-    @posts = @user.posts.where(posted_on: @selected_date)
+    @posts = posts.where(posted_on: @selected_date)
+                  .order(posted_on: :desc)
   else
-    @posts = @user.posts
+    @posts = posts.order(posted_on: :desc)
   end
 end
+
   def edit
     @user = current_user
   end
